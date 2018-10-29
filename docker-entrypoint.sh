3b285f8f544ec9e4b3be60607fb2edb4ac9c0f41
Joakim Brorsson <b.joakim@gmail.com>
2018-10-29 16:14:20 +0100

    minor

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo $PATH
 chown -R 1000:1000 /omnet
-omnetpp
+#export DEFAULT_WORKSPACE_ARGS="‐vmargs ‐Dos‐gi.instance.area.default=/workspace"
+omnetpp 
 
 while true; do echo "running container"; test $? -gt 128 && break; sleep 9; done
 
