a5c4ce1523ebdc06c2d29788d309758a2c274a5c
Joakim Brorsson <b.joakim@gmail.com>
2018-11-27 11:42:28 +0100

    running artery from startup script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,10 @@
 #start omnet
 chown -R 1000:1000 /omnet
 #export DEFAULT_WORKSPACE_ARGS="‐vmargs ‐Dos‐gi.instance.area.default=/workspace"
-omnetpp
+#omnetpp
+
+#run veins
+cd artery/build && cmake --build . --target run_example -j 5
 
 #keep container alive
 while true; do echo "running container"; test $? -gt 128 && break; sleep 9; done
