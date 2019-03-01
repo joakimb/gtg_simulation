04c31acf3cbc5d99dd96060baa6df7ff5ab77988
Joakim Brorsson <joakimb@Joakims-MacBook-Pro.local>
2019-03-01 16:59:28 +0100

    feature: Test correct encoding of Pseud message

--- code/dissemination/test/GTGTest.cc
+++ code/dissemination/test/GTGTest.cc
@@ -3,6 +3,7 @@
 #include "../src/GTGMessage.h"
 #include "../src/PseudMessage.h"
 #include "../src/ShareMessage.h"
+#include "../src/base64.h"
 #include <string>
 #include <vector>
 
@@ -13,6 +14,24 @@ TEST_CASE("PseudReturnsCorrectTypeTest"){
 
 	PseudMessage msg (pseud);
     REQUIRE(msg.getType() == "GTG_PSEUD");
+}
+
+TEST_CASE("PseudMessageEncodesCorrectlyTest"){
+
+	std::string pseudString = "dummypseud";
+    std::vector<unsigned char> pseud (pseudString.begin(), pseudString.end());
+
+	PseudMessage msg (pseud);
+	std::string encodedMessage = msg.getEncoded();
+
+    std::string cborData = base64_decode(encodedMessage);
+	json json = json::from_cbor(cborData);
+    std::string type = json.at("gtg_type");
+    std::vector<unsigned char> pseudVectDecoded = json.at("gtg_pseud");
+    std::string pseudStringDecoded(pseudVectDecoded.begin(), pseudVectDecoded.end());
+
+    REQUIRE(type == "GTG_PSEUD");
+    REQUIRE(pseudStringDecoded == pseudString);
 }
 
 TEST_CASE("ShareReturnsCorrectTypeTest"){
